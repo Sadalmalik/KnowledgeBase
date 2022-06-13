ef81de103b3222bfe67db842267d98dc909ffbed
Gleb Kuzmin <gleb.kuzmin@geeky.house>
2022-06-14 00:11:31 +0300

    Cleanup;

--- main.py
+++ main.py
@@ -8,29 +8,14 @@ def main():
 
     print(f"Before:\n{str(database)}\n")
 
-    i = 100
-    while i > 0:
-        i -= 1
+    i = 0
+    while i < 100:
+        i += 1
         changes = apply_rules(database)
         if changes == 0:
             break
 
-    print(f"After {100 - i}:\n{str(database)}\n")
-    # print(f"\n\n")
-    # dump_facts(test['facts'], "before")
-    # apply_rules(test['facts'], test['rules'])
-    # dump_facts(test['facts'], "after")
-    #
-    # print(f"\n\n")
-    # dump_facts(database['facts'], "before")
-    # i = 100
-    # while i > 0:
-    #     i -= 1
-    #     if 0 == apply_rules(database['facts'], database['rules']):
-    #         break
-    #     dump_facts(database['facts'], f"inner {i}")
-    # print(f"Steps: {i}")
-    # dump_facts(database['facts'], "after")
+    print(f"After {i}:\n{str(database)}\n")
 
 
 if __name__ == '__main__':
